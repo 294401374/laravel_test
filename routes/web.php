d1d4dc858825998d245e632e2f917063f21ea5b9
baotong <294401374@qq.com>
2020-07-17 18:54:03 +0800

    获取请求参数

--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,18 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+use \Illuminate\Http\Request;
 Route::get('/', function () {
     return view('welcome');
 });
 Route::get('user/{id}', 'UserController@show');
 
 Route::get('test1', function(\Illuminate\Http\Request $request){
+    
+    $query = Request::query();
+    dump(Request::query('query_name1'));
+    dd($query);
+    
     dump($request->path());
     dump($request->url());
     dump($request->fullUrl());
@@ -39,3 +44,38 @@ Route::post('test2', function(){
     // $input = $request->input('input');
     // dump($input);
 });
+
+// 用input获取参数
+Route::post('test3', function(\Illuminate\Http\Request $request){
+    // $all = $request->all();
+    $input = Request::input();
+    // dd($input);
+    $input = Request::input('array.1.name');
+    dump($input);
+});
+
+Route::post('json', function(Request $request){
+    $name = $request->input('json.name');
+    dump($name);
+    $input = $request->input();
+    dd($input['json']);
+});
+
+Route::post('boolean', function(Request $request){
+    $bool = $request->boolean('bool');
+    dd($bool);
+});
+Route::post('except', function(Request $request){
+    $only = $request->only(['bool', 'false']);
+    $except = $request->except('json');
+    $fill = $request->filled('name');
+    $miss = $request->missing("name");
+    $name = $request->old('name');
+    dump($name);
+    dump($name);
+    dump($miss);
+    dump($fill);
+    dump($except);
+    dd($only);
+});
+
