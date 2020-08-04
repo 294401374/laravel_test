bd79de13aa8aa61596d983dc505dbe0ce40e8ce4
baotong <294401374@qq.com>
2020-08-04 11:13:55 +0800

    controller 对应到控制器中保证简洁

--- routes/web.php
+++ routes/web.php
@@ -29,64 +29,15 @@ Route::get('collection', 'CollectionController@collectionMethed');
 //1. controller
 Route::get('user/{id}', 'UserController@show');
 
-Route::get('test1', function(\Illuminate\Http\Request $request){
-    
-    $query = Request::query();
-    dump(Request::query('query_name1'));
-    dd($query);
-    
-    dump($request->path());
-    dump($request->url());
-    dump($request->fullUrl());
-    dump($request->method());
-    $all = $request->all();
-    dump($all);
-   $input = $request->input('input');
-   dump($input);
-});
+Route::get('test1', 'RouteController@test2');
 
-Route::post('test2', function(){
-   dump(1111);
-    // dump($request->url());
-    // dump($request->fullUrl());
-    // dump($request->method());
-    // $all = $request->all();
-    // dump($all);
-    // $input = $request->input('input');
-    // dump($input);
-});
+Route::post('test2', 'RouteController@test2');
 
 // 用input获取参数
-Route::post('test3', function(\Illuminate\Http\Request $request){
-    // $all = $request->all();
-    $input = Request::input();
-    // dd($input);
-    $input = Request::input('array.1.name');
-    dump($input);
-});
+Route::post('test3', 'RouteController@test3');
 
-Route::post('json', function(Request $request){
-    $name = $request->input('json.name');
-    dump($name);
-    $input = $request->input();
-    dd($input['json']);
-});
+Route::post('json', 'RouteController@JsonResponse');
 
-Route::post('boolean', function(Request $request){
-    $bool = $request->boolean('bool');
-    dd($bool);
-});
-Route::post('except', function(Request $request){
-    $only = $request->only(['bool', 'false']);
-    $except = $request->except('json');
-    $fill = $request->filled('name');
-    $miss = $request->missing("name");
-    $name = $request->old('name');
-    dump($name);
-    dump($name);
-    dump($miss);
-    dump($fill);
-    dump($except);
-    dd($only);
-});
+Route::post('boolean', 'RouteController@BooleanResponse');
+Route::post('except', 'RouteController@ExceptResponse');
 
